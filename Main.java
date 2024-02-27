dba19b6dba27f81cec90f2105de1266546dcf203
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2024-02-27 16:11:20 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ public class Main
 {
     public static void main(String[] args) 
     {
+        // Class Object Creation
         QuetionService service=new QuetionService();
         service.displayQuetions();
-        
         service.Printscore();
     }
 }
