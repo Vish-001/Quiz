6a0c087d16b871881c302e01263b7772eb979877
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2024-02-28 11:10:44 +0530

    Update QuetionService.java

--- QuetionService.java
+++ QuetionService.java
@@ -14,6 +14,7 @@ public class QuetionService
    void displayQuetions()
    {
       int i=0;
+      
       for(Quetion p:quetion)
       {
          System.out.print("que no:" +p.getId());
@@ -27,6 +28,7 @@ public class QuetionService
    }
    void Printscore()
    {
+      // To calulate score
       int score=0;
       for(int i=0;i<3;i++)
       {
