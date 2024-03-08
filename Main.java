5aec619f0e05dfdd2b8edfab3e3cc1308c0ca57b
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2024-03-08 14:44:08 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        // Class Object Creation
         QuetionService service=new QuetionService();
         service.displayQuetions();
         
